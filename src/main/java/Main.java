670d8619a2043bc4ffc05e78eca0c6139de29263
Thomas Czachorowski <Thomas.Czachorowski@cdpipelinedevshop.com>
2022-08-17 10:39:18 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        //Use 8 print statements to print a smiley face.
+        //Your art will rely on only a single character, besides space, such as X or #.
+        System.out.println(" /|\\   /|\\ ");
+        System.out.println(" |||   |||  ");
+        System.out.println("/^^^---^^^\\");
+        System.out.println("< 0     0 > ");
+        System.out.println("<    v    > ");
+        System.out.println("< (__|__) > ");
+        System.out.println("<   u u   > ");
+        System.out.println("\\         / ");
+        System.out.println("Scary Bunny");
+
+    }
+}
+
+
